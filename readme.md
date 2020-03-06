fdf0145ae167c5587809db5d17894f44b19feb4b
Chris Jenkins <chris.jenkins@masabi.com>
2020-03-06 11:24:46 +0000

    Make the main method callable from Gradle.

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Bogus Personality Test
+
+This application is intended to work out your DISA personality type. Unfortunately,
+it is utterly unscientific and should not be relied up to give a correct or useful
+answer.
+
+# Usage
+
+Run it as follows:
+
+   ./gradlew run -q --console=plain 
+
+It will show a sequence of traits (by default 25) chosen at random. For each one,
+answer y or n. It will then tell you how strongly you display each of the four DISA
+personality types.
+
+Again, this application should not be relied upon to give correct or useful results.
+It's probably all a load of rubbish.
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,23 @@
 fun main(args: Array<String>) {
-
-    val numberOfQuestions = 10
-
+    val numberOfQuestions = 25
 
     val traits = Trait.values().toList().shuffled().take(numberOfQuestions)
 
-    val responses = traits.map { trait ->
+    val positiveResponses = traits.map { trait ->
         val response: Boolean = askOneQuestion(trait.toString())
         trait to response
     }
         .filter { it.second}
         .map { it.first.toString() }
 
-    val scores: PersonalityResult = PersonalityDecider().computeScores(* responses.toTypedArray<String>())
-
-    println(scores)
+    val scores: PersonalityResult = PersonalityDecider().computeScores(* positiveResponses.toTypedArray<String>())
 
     println("Here are your scores:")
     PersonalityType.values().forEach {
         val type = it
         val score = scores.getScore(it)
 
-        val thingie = score.toDouble() / numberOfQuestions
+        val thingie = score.toDouble() / positiveResponses.size
         println("$type: $thingie")
     }
 }
